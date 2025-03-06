2ebbb23c0213316a0209085d84f27ccee85864f9
LeafYeeXYZ <leafyeexyz@outlook.com>
2025-03-06 11:06:42 +0800

    feat: 为 AI 助手和 R 语言服务器设置提供更多引导

--- src/components/data/PreviewData.tsx
+++ src/components/data/PreviewData.tsx
@@ -19,7 +19,7 @@ import {
   SaveOutlined,
   CloudServerOutlined,
   CommentOutlined,
-  LoadingOutlined,
+  InfoCircleOutlined,
 } from '@ant-design/icons'
 import { AgGridReact } from 'ag-grid-react'
 import 'ag-grid-community/styles/ag-grid.css'
@@ -118,9 +118,19 @@ export function PreviewData() {
           <Popover
             title={
               <span>
-                AI辅助分析设置 {'['}当前状态: {ai ? '可用' : '不可用'}
-                <LoadingOutlined className='mx-[0.15rem] scale-75' />
-                {']'}
+                AI辅助分析设置 {'|'} 当前状态: {ai ? <Tag color='green'>可用</Tag> : <Tag color='red'>不可用</Tag>}
+                <Popover
+                  content={(
+                    <div className='flex flex-col gap-1'>
+                      <div>
+                        在输入全部信息后, PsychPen 会自动验证 AI 服务是否可用
+                      </div>
+                      <div>
+                        如果数秒后仍未显示可用, 请检查网络连接和信息是否填写正确
+                      </div>
+                    </div>
+                  )}
+                ><InfoCircleOutlined /></Popover>
               </span>
             }
             trigger={['click', 'hover']}
@@ -129,7 +139,12 @@ export function PreviewData() {
             <Button icon={<CommentOutlined />}>AI辅助分析设置</Button>
           </Popover>
           <Popover
-            title='R语言服务器设置'
+            title={
+              <span>
+                R语言服务器设置{' '}
+                <Popover content='详见使用文档或询问 AI 助手'><InfoCircleOutlined /></Popover>
+              </span>
+            }
             trigger={['click', 'hover']}
             content={<ConfigR />}
           >
@@ -171,8 +186,8 @@ function ConfigAI() {
     FALSE = '关闭AI辅助分析',
   }
   return (
-    <div className='flex flex-col w-96'>
-      <div className='mb-4'>
+    <div className='flex flex-col w-96 gap-2'>
+      <div className='mb-2'>
         <Segmented
           block
           className='border dark:border-[#424242]'
@@ -181,7 +196,28 @@ function ConfigAI() {
           onChange={(value) => _DataView_setOpenaiEnable(value === Open.TRUE)}
         />
       </div>
-      <div className='mb-4'>
+      <p className='w-full text-left pl-1'>
+        API 地址 <Tag>baseUrl</Tag>
+        <Popover
+          content={(
+            <div className='flex flex-col gap-1'>
+              <div>
+                请填写各大模型提供商的 OpenAI 兼容 API 地址
+              </div>
+              <div>
+                对于 <Tag style={{ margin: 0 }}>DeepSeek</Tag> 是 <Tag style={{ margin: 0 }}>https://api.deepseek.com/v1</Tag>
+              </div>
+              <div>
+                对于 <Tag style={{ margin: 0 }}>Qwen</Tag> 是 <Tag style={{ margin: 0 }}>https://dashscope.aliyuncs.com/compatible-mode/v1</Tag>
+              </div>
+              <div>
+                对于 <Tag style={{ margin: 0 }}>OpenAI</Tag> 是 <Tag style={{ margin: 0 }}>https://api.openai.com/v1</Tag>
+              </div>
+            </div>
+          )}
+        ><InfoCircleOutlined /></Popover>
+      </p>
+      <div className='mb-2'>
         <Input
           placeholder='请输入OpenAI兼容API端点 (baseUrl)'
           defaultValue={openaiEndpoint}
@@ -189,7 +225,10 @@ function ConfigAI() {
           onChange={(e) => _DataView_setOpenaiEndpoint(e.target.value ?? '')}
         />
       </div>
-      <div className='mb-4'>
+      <p className='w-full text-left pl-1'>
+        API 密钥 <Tag>apiKey</Tag>
+      </p>
+      <div className='mb-2'>
         <Input.Password
           placeholder='请输入OpenAI兼容API密钥 (apiKey)'
           defaultValue={openaiApiKey}
@@ -197,7 +236,25 @@ function ConfigAI() {
           onChange={(e) => _DataView_setOpenaiApiKey(e.target.value ?? '')}
         />
       </div>
-      <div className='mb-4'>
+      <p className='w-full text-left pl-1'>
+        AI 模型名称 <Tag>modelId</Tag>
+        <Popover
+          content={(
+            <div className='flex flex-col gap-1'>
+              <div>
+                请填写您所使用的 AI 模型的 ID (模型必须支持 <Tag style={{ margin: 0 }}>Function Calling</Tag> 功能)
+              </div>
+              <div>
+                如 <Tag style={{ margin: 0 }}>DeepSeek-V3</Tag> (ID 为 <Tag style={{ margin: 0 }}>deepseek-chat</Tag>), <Tag style={{ margin: 0 }}>Qwen</Tag> (ID 为 <Tag style={{ margin: 0 }}>qwen-max</Tag>), <Tag style={{ margin: 0 }}>GPT-4o</Tag> (ID 为 <Tag style={{ margin: 0 }}>gpt-4o</Tag>) 等
+              </div>
+              <div>
+                不建议使用 <Tag style={{ margin: 0 }}>DeepSeek-R1</Tag>, <Tag style={{ margin: 0 }}>QwQ</Tag>, <Tag style={{ margin: 0 }}>o3-mini</Tag> 等推理模型
+              </div>
+            </div>
+          )}
+        ><InfoCircleOutlined /></Popover>
+      </p>
+      <div className='mb-2'>
         <Input
           placeholder='请输入AI模型名称 (modelId)'
           defaultValue={model}
@@ -205,15 +262,17 @@ function ConfigAI() {
           onChange={(e) => _DataView_setModel(e.target.value ?? '')}
         />
       </div>
-      <p className='w-full text-xs text-center px-2 mb-1'>
-        如果启用AI辅助分析功能, 则在与AI交互时
-      </p>
-      <p className='w-full text-xs text-center px-2 mb-1'>
-        部分数据信息将上传至上面指定的AI服务
-      </p>
-      <p className='w-full text-xs text-center px-2 mb-1'>
-        如果使用的不是自部署AI服务, 请注意数据安全
-      </p>
+      <div className='flex flex-col gap-1'>
+        <p className='w-full text-xs text-center px-2'>
+          如果启用AI辅助分析功能, 则在与AI交互时
+        </p>
+        <p className='w-full text-xs text-center px-2'>
+          部分数据信息将上传至上面指定的AI服务
+        </p>
+        <p className='w-full text-xs text-center px-2'>
+          如果使用的不是自部署AI服务, 请注意数据安全
+        </p>
+      </div>
     </div>
   )
 }
@@ -232,8 +291,8 @@ function ConfigR() {
     FALSE = '关闭R语言服务器',
   }
   return (
-    <div className='flex flex-col w-96'>
-      <div className='mb-4'>
+    <div className='flex flex-col w-96 gap-2'>
+      <div className='mb-2'>
         <Segmented
           block
           className='border dark:border-[#424242]'
@@ -242,7 +301,10 @@ function ConfigR() {
           onChange={(value) => _DataView_setRenable(value === Open.TRUE)}
         />
       </div>
-      <div className='mb-4'>
+      <p className='w-full text-left pl-1'>
+        服务器地址
+      </p>
+      <div className='mb-2'>
         <Input
           placeholder='请输入服务器地址'
           defaultValue={Rurl}
@@ -250,7 +312,10 @@ function ConfigR() {
           onChange={(e) => _DataView_setRurl(e.target.value ?? '')}
         />
       </div>
-      <div className='mb-4'>
+      <p className='w-full text-left pl-1'>
+        服务器密码
+      </p>
+      <div className='mb-2'>
         <Input.Password
           placeholder='请输入服务器密码'
           defaultValue={Rpassword}
@@ -258,15 +323,17 @@ function ConfigR() {
           onChange={(e) => _DataView_setRpassword(e.target.value ?? '')}
         />
       </div>
-      <p className='w-full text-xs text-center px-2 mb-1'>
-        如果启用R语言服务器功能, 则在执行部分统计功能时
-      </p>
-      <p className='w-full text-xs text-center px-2 mb-1'>
-        数据将上传至上面填写的R语言服务器进行处理
-      </p>
-      <p className='w-full text-xs text-center px-2 mb-1'>
-        如果使用的不是官方或自部署服务器, 请注意数据安全
-      </p>
+      <div className='flex flex-col gap-1'>
+        <p className='w-full text-xs text-center px-2'>
+          如果启用R语言服务器功能, 则在执行部分统计功能时
+        </p>
+        <p className='w-full text-xs text-center px-2'>
+          数据将上传至上面填写的R语言服务器进行处理
+        </p>
+        <p className='w-full text-xs text-center px-2'>
+          如果使用的不是官方或自部署服务器, 请注意数据安全
+        </p>
+      </div>
     </div>
   )
 }
